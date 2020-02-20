e972783290c3bc8ab621997f84c778850409bce6
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2020-02-20 14:57:58 -0300

    Problema do editor

--- src/main/resources/sqlSaci/findAbreviacoes.sql
+++ src/main/resources/sqlSaci/findAbreviacoes.sql
@@ -1,7 +1,5 @@
 SELECT MID(localizacao, 1, 4) AS abreviacoes
 FROM sqldados.prdloc
-WHERE prdno = LPAD(:prdno, 16, ' ') AND
-      grade = :grade
 GROUP BY abreviacoes
 HAVING abreviacoes LIKE 'CD%' AND
        abreviacoes NOT LIKE 'CD00' AND
