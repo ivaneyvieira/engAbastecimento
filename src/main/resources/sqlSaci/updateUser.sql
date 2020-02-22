28b10a3d67d9ed9133f830bfd6e678199ec93bd2
Ivaney Vieira de Sales <ivaneyvieira@gmail.com>
2020-02-22 11:03:08 -0300

    Adicionar todas as localizações

--- src/main/resources/sqlSaci/updateUser.sql
+++ src/main/resources/sqlSaci/updateUser.sql
@@ -1,7 +1,6 @@
 INSERT IGNORE INTO sqldados.abastecimentoLoc(no, abreviacoes)
 SELECT no, auxStr
-FROM sqldados.users
-WHERE bits2 & pow(2, 5) <> 0;
+FROM sqldados.users;
 
 UPDATE sqldados.abastecimentoLoc AS L INNER JOIN sqldados.users AS U USING (no)
 SET bits2       = :bitAcesso,
